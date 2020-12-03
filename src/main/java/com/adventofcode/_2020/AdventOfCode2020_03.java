9e882874abc3c623189e0170e7eafbfba5746852
Himank Batra <himank2010@gmail.com>
2020-12-03 14:15:34 +0530

    add day 03 solution

--- src/main/java/com/adventofcode/_2020/AdventOfCode2020_03.java
+++ src/main/java/com/adventofcode/_2020/AdventOfCode2020_03.java
@@ -1,5 +1,6 @@
 package com.adventofcode._2020;
 
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -7,16 +8,36 @@ import java.util.List;
 
 public class AdventOfCode2020_03 {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         String inputFile = "2020/input_03.txt";
         List<String> input;
         try {
-            input = Files.readAllLines(Paths.get("src/main/resources",inputFile));
+            input = Files.readAllLines(Paths.get("src/main/resources", inputFile));
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
 
-
+        int noOfTrees = 0;
+        int index = 0;
+        int loop = 0;
+        for (String i : input) {
+            char[] chars = i.toCharArray();
+           while(chars.length <= index) {
+                chars = new String(chars).concat(i).toCharArray();
+            }
+            loop++;
+           // for part 2
+           /* if(loop%2==0)
+            {
+                continue;
+            }*/
+            if (chars[index] == '#') {
+                System.out.println("loop -> " + loop + " index -> " + index + " string -> " + new String(chars));
+                noOfTrees++;
+            }
+            index = index + 7;
+        }
+        System.out.println("noOfTrees = " + noOfTrees);
 
     }
 }
