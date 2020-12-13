4d02a7efe8cbb7592db38cc6036b2435a0ff8338
Himank Batra <himank2010@gmail.com>
2020-12-13 12:48:39 +0530

    add solution day 13

--- src/main/java/com/adventofcode/_2020/AdventOfCode13.java
+++ src/main/java/com/adventofcode/_2020/AdventOfCode13.java
@@ -4,7 +4,9 @@ package com.adventofcode._2020;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class AdventOfCode13 {
@@ -19,8 +21,46 @@ public class AdventOfCode13 {
             throw new RuntimeException(exception);
         }
 
+        int timestamp = Integer.parseInt(input.get(0));
+        List<Integer> busIds = Arrays.stream(input.get(1).split(","))
+                .filter(b -> !b.equals("x"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        int min = Integer.MAX_VALUE;
+        int minBusId = 0;
+        for (int busId : busIds) {
+            int time = timestamp;
+            while (time % busId != 0) {
+                time++;
+            }
+            if (time < min) {
+                min = time;
+                minBusId = busId;
+            }
 
+        }
+        System.out.println("result 1 => " + ((min - timestamp) * minBusId));
+
+
+        long time = 0;
+        long factor = 1;
+        String[] split = input.get(1).split(",");
+        for (int i = 0; i < split.length; ) {
+            String b = split[i];
+            if (b.equals("x")) {
+                i++;
+            } else {
+                int busId = Integer.parseInt(b);
+                if ((time + i) % busId == 0) {
+                    factor *= busId;
+                    i++;
+                } else {
+                    time += factor;
+                }
+            }
+        }
+
+        System.out.println("result 2 => " + time);
 
     }
-
 }
