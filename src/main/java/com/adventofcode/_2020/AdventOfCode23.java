6810a8cbc40082c97f793ea081e108010f0fbde3
Himank Batra <himank2010@gmail.com>
2020-12-24 14:24:17 +0530

    add solution day 24

--- src/main/java/com/adventofcode/_2020/AdventOfCode23.java
+++ src/main/java/com/adventofcode/_2020/AdventOfCode23.java
@@ -41,6 +41,8 @@ public class AdventOfCode23 {
         for (int i = 0; i < 100; i++) {
             System.out.println("i " + i);
 
+
+
             CircleNode<Integer> ccup = cups.get(curr);
             CircleNode<Integer> a = ccup.next.remove();
             CircleNode<Integer> b = ccup.next.remove();
@@ -67,10 +69,61 @@ public class AdventOfCode23 {
             o = o.next;
         }
 
+        String calc = new AdventOfCode23().calc(input);
+        System.out.println("calc = " + calc);
+
         new AdventOfCode23().part2(input.get(0));
+
+        long l = new AdventOfCode23().calc2(input);
+        System.out.println("l = " + l);
 
 
     }
+
+
+
+    private String calc(List<String> strs) {
+        String now = strs.get(0);
+        for (int i = 0; i < 100; i++) {
+            now = move(now);
+            System.out.println("now = " + now);
+        }
+        int index = now.indexOf("1");
+        return now.substring(index + 1) + now.substring(0, index);
+    }
+
+    private String move(String from) {
+        List<Integer> g = new ArrayList<>();
+        for (int i = 0; i < from.length(); i++) {
+            char c = from.charAt(i);
+            g.add(c - '0');
+        }
+        List<Integer> pickup = new ArrayList<>();
+        pickup.add(g.remove(1));
+        pickup.add(g.remove(1));
+        pickup.add(g.remove(1));
+        int destination = g.get(0) - 1;
+        if (destination == 0) {
+            destination = 9;
+        }
+        while (pickup.contains(destination)) {
+            destination--;
+            if (destination == 0) {
+                destination = 9;
+            }
+        }
+        int destIndex = g.indexOf(destination);
+        g.add(destIndex+1, pickup.get(2));
+        g.add(destIndex+1, pickup.get(1));
+        g.add(destIndex+1, pickup.get(0));
+        String s = "";
+        for(int i=1;i<g.size();i++){
+            s += g.get(i);
+        }
+        s += g.get(0);
+        return s;
+    }
+
 
     private void part2(String input)
     {
@@ -129,6 +182,76 @@ public class AdventOfCode23 {
         System.out.println(a * b);
     }
 
+
+
+    Node[] map = new Node[1000001];
+
+    private long calc2(List<String> strs) {
+        String now = strs.get(0);
+
+        List<Integer> g = new LinkedList<>();
+        for (int i = 0; i < now.length(); i++) {
+            char c = now.charAt(i);
+            g.add(c - '0');
+        }
+        while (g.size() < 1000000) {
+            g.add(g.size() + 1);
+        }
+        List<Node> nodes = new LinkedList<>();
+        Node lastNode = null;
+        for (Integer value : g) {
+            final Node node = new Node();
+            node.value = value;
+            map[value] = node;
+            if (lastNode != null) {
+                lastNode.next = node;
+            }
+            nodes.add(node);
+            lastNode = node;
+        }
+        lastNode.next = nodes.get(0);
+
+        Node current = nodes.get(0);
+
+        for (int i = 0; i < 10000000; i++) {
+            current = move(current);
+        }
+
+        Node one = map[1];
+        Node next = one.next;
+        Node next2 = next.next;
+        long svar = next.value;
+        svar *= next2.value;
+        return svar;
+    }
+
+    private Node move(Node node) {
+        Node pickup = node.next;
+        node.next = node.next.next.next.next;
+        List<Integer> pickupValues = new ArrayList<>();
+        pickupValues.add(pickup.value);
+        pickupValues.add(pickup.next.value);
+        pickupValues.add(pickup.next.next.value);
+        int destination = node.value - 1;
+        if (destination == 0) {
+            destination = 1000000;
+        }
+        while (pickupValues.contains(destination)) {
+            destination--;
+            if (destination == 0) {
+                destination = 1000000;
+            }
+        }
+        Node dest = map[destination];
+        pickup.next.next.next = dest.next;
+        dest.next = pickup;
+        return node.next;
+    }
+
+    private static class Node {
+        int value;
+        Node next;
+    }
 
 
 }
--- src/main/java/com/adventofcode/_2020/AdventOfCode24.java
+++ src/main/java/com/adventofcode/_2020/AdventOfCode24.java
@@ -4,7 +4,10 @@ package com.adventofcode._2020;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 
 public class AdventOfCode24 {
@@ -19,10 +22,191 @@ public class AdventOfCode24 {
             throw new RuntimeException(exception);
         }
 
+        long calc = new AdventOfCode24().calc(input);
+        System.out.println("calc = " + calc);
 
+        long calc2 = new AdventOfCode24().calc2(input);
+        System.out.println("calc2 = " + calc2);
 
+    }
+
+    Map<Pair<Integer, Integer>, Boolean> g = new HashMap<>();
+
+    private long calc(List<String> strs) {
+        long sum = 0;
+        for (String str : strs) {
+            Pair<Integer, Integer> pos = go(str);
+            g.put(pos, !g.getOrDefault(pos, false));
+        }
+        for (Boolean value : g.values()) {
+            if (value) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+    private Pair<Integer, Integer> go(String str) {
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'e') {
+                x++;
+            } else if (c == 'w') {
+                x--;
+            } else if (c == 'n') {
+                char d = str.charAt(++i);
+                if (d == 'w') {
+                    x--;
+                    y++;
+                } else if (d == 'e') {
+                    y++;
+                }
+            } else if (c == 's') {
+                char d = str.charAt(++i);
+                if (d == 'w') {
+                    y--;
+                } else if (d == 'e') {
+                    x++;
+                    y--;
+                }
+            }
+        }
+        return Pair.with(y, x);
     }
 
 
 
+    private long calc2(List<String> strs) {
+        Map<Pair<Integer, Integer>, Boolean> g = new HashMap<>();
+        for (String str : strs) {
+            Pair<Integer, Integer> pos = go2(str);
+            g.put(pos, !g.getOrDefault(pos, false));
+        }
+        for (int i = 0; i < 100; i++) {
+            System.out.println("i = " + i);
+            g = flip(g);
+        }
+        long sum = 0;
+        for (Boolean value : g.values()) {
+            if (value) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+    int dx[] = {1, -1, -1, 0, 0, 1};
+    int dy[] = {0, 0, 1, 1, -1, -1};
+
+    private Map<Pair<Integer, Integer>, Boolean> flip(Map<Pair<Integer, Integer>, Boolean> from) {
+        Map<Pair<Integer, Integer>, Boolean> to = new HashMap<>();
+        for (int y = -150; y <= 150; y++) {
+            for (int x = -150; x <= 150; x++) {
+                final Boolean now = from.getOrDefault(Pair.with(y, x), false);
+                int adjacent = 0;
+                for (int dir = 0; dir < dx.length; dir++) {
+                    int xx = x + dx[dir];
+                    int yy = y + dy[dir];
+                    if (from.getOrDefault(Pair.with(yy, xx), false)) {
+                        adjacent++;
+                    }
+                }
+                if (now) {
+                    if (adjacent == 0 || adjacent > 2) {
+                        to.put(Pair.with(y, x), false);
+                    } else {
+                        to.put(Pair.with(y, x), true);
+                    }
+                } else {
+                    if (adjacent == 2) {
+                        to.put(Pair.with(y, x), true);
+                    } else {
+                        to.put(Pair.with(y, x), false);
+                    }
+                }
+            }
+        }
+        return to;
+    }
+
+    private Pair<Integer, Integer> go2(String str) {
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'e') {
+                x++;
+            } else if (c == 'w') {
+                x--;
+            } else if (c == 'n') {
+                char d = str.charAt(++i);
+                if (d == 'w') {
+                    x--;
+                    y++;
+                } else if (d == 'e') {
+                    y++;
+                }
+            } else if (c == 's') {
+                char d = str.charAt(++i);
+                if (d == 'w') {
+                    y--;
+                } else if (d == 'e') {
+                    x++;
+                    y--;
+                }
+            }
+        }
+        return Pair.with(y, x);
+    }
+
+}
+class Pair<T, R> {
+
+    private T key;
+    private R value;
+
+    public Pair() {
+    }
+
+
+    public Pair(T key, R value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public static <T, R> Pair<T, R> with(T key, R value) {
+        return new Pair<>(key, value);
+    }
+
+    public T getKey() {
+        return key;
+    }
+
+    public void setKey(T key) {
+        this.key = key;
+    }
+
+    public R getValue() {
+        return value;
+    }
+
+    public void setValue(R value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(key, pair.key) &&
+                Objects.equals(value, pair.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
 }
