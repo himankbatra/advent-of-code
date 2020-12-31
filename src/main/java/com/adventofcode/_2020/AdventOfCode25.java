03a10811d4475bfb594c7bf95d93c09aaf394747
Himank Batra <himank2010@gmail.com>
2020-12-31 16:31:29 +0530

    add solution day 25

--- src/main/java/com/adventofcode/_2020/AdventOfCode25.java
+++ src/main/java/com/adventofcode/_2020/AdventOfCode25.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class AdventOfCode25 {
@@ -12,17 +13,25 @@ public class AdventOfCode25 {
 
     public static void main(String[] args) {
         String inputFile = "2020/input25.txt";
-        List<String> input;
+        List<Integer> input;
         try {
-            input = Files.readAllLines(Paths.get("src/main/resources", inputFile));
+            input = Files.lines(Paths.get("src/main/resources", inputFile))
+                    .map(Integer::parseInt).collect(Collectors.toList());
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
 
+        long val0 = 1;
+        long val1 = 1;
+        while (val0 != input.get(0)) {
+            val0 = (val0 * 7) % 20201227;
+            val1 = (val1 * input.get(1)) % 20201227;
+        }
+
+        System.out.println("val1 = " + val1);
 
 
     }
-
 
 
 }
